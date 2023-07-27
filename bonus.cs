484320671d1fcfb0c3dfa5c88c44431b1580df3a
SivaBharat <sivabharthece@gmail.com>
2023-07-27 14:58:25 +0530

    bonusCommit

--- bonus.cs
+++ bonus.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,23 +11,43 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            int i, num,id;
-            double bonus,salary;
-            string name;
-            Console.WriteLine("enter the number of employees:");
-            num=Convert.ToInt32(Console.ReadLine());
-            for(i=0; i < num;i++)
+            int i, num;
+            Console.WriteLine("Enter the number of employees:");
+            num = Convert.ToInt32(Console.ReadLine());
+            int[] id = new int[num];
+            string[] name = new string[num];
+            double[] salary = new double[num];
+            double[] bonus = new double[num];
+
+            for (i = 0; i < num; i++)
             {
                 Console.WriteLine($"Enter the employee {i + 1} id ");
-                id = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($"Enter the employee {i + 1} name ");
-                name= Console.ReadLine();
-                Console.WriteLine($"Enter the employee {i+1} salary ");
-                salary=Convert.ToInt32(Console.ReadLine());
-                bonus=Convert.ToInt32(Console.ReadLine());    
+                id[i] = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine($"Enter the employee {i + 1} name ");
+                name[i] = Console.ReadLine();
+
+                Console.WriteLine($"Enter the employee {i + 1} salary ");
+                salary[i] = Convert.ToDouble(Console.ReadLine());
+
+               
+                CalculateBonus(salary[i], out bonus[i]);
             }
 
+            Console.WriteLine("Employee Details:");
+            for (i = 0; i < num; i++)
+            {
+                Console.WriteLine("Employee id: " + id[i]);
+                Console.WriteLine("Employee name: " + name[i]);
+                Console.WriteLine("Employee salary: " + salary[i]);
+                Console.WriteLine("Employee bonus: " + bonus[i]);
+                Console.WriteLine("-------------------------------");
+            }
+        }
+
+        static void CalculateBonus(double salary, out double bonus)
+        {            
+            bonus = 0.10 * salary;
         }
     }
 }
