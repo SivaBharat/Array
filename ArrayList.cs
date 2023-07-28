faada2149843c5e57a0544f7ce1767a5d5ca26ad
SivaBharat <sivabharthece@gmail.com>
2023-07-28 12:39:55 +0530

    arraylistCommit

--- ArrayList.cs
+++ ArrayList.cs
@@ -1,42 +1,38 @@
 ﻿using System;
 using System.Collections;
+using System.ComponentModel;
 
 namespace Array
 {
-    internal class ArrayLis
+    public class ArrayLis
     {
-        static void Main()
+        public void display()
         {
-            ArrayList mixedList = new ArrayList();
-            mixedList.Add("Siva");
-            mixedList.Add(null);
-            mixedList.Add("Arun");
-            mixedList.Add(null);
-            mixedList.Add("Karthi");
-            mixedList.Add(null);
-
-            Console.WriteLine("Original ArrayList:");
-            PrintArrayList(mixedList);
-
-            
-            for (int i = 0; i < mixedList.Count; i++)
+            ArrayList arrayList = new ArrayList();
+            for(int i = 0; i < 5; i++)
             {
-                if (mixedList[i] == null)
+                Console.WriteLine($"Enter the value {i + 1}");
+                var j=Console.ReadLine();
+                if (j == "")
                 {
-                    mixedList[i] = "I have a value";
+                    arrayList.Add(null);
                 }
+                arrayList.Add(j);   
             }
-
-            Console.WriteLine("\nUpdated ArrayList:");
-            PrintArrayList(mixedList);
-        }
-
-        static void PrintArrayList(ArrayList list)
-        {
-            foreach (var item in list)
+            foreach(object  obj in arrayList)
             {
-                Console.WriteLine(item);
+                if(obj == null)
+                {
+                    Console.WriteLine("I have a value");
+                    continue;
+                }
+                Console.WriteLine(obj.ToString());
             }
+        }
+        static void Main(string[] args)
+        {
+            ArrayLis arrayLis = new ArrayLis();
+            arrayLis.display();
         }
     }
 }
