9997a0940a236a3fcdb0ab69e5f13c88e6c6a61f
SivaBharat <sivabharthece@gmail.com>
2023-08-11 16:30:01 +0530

    AsyncCommit

---
+++ Asyncdemo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Asyncdemo
+    {
+        static int sum = 0;
+        static int mul=1;
+        static async Task<int> Sum(int n)
+        {          
+            
+            for (int i = 1; i < n;i++)
+            {
+                sum += i;
+            }
+            await Task.Delay(5000);
+            return sum;
+            
+        }
+        static async Task<int> Mul(int n)
+        {           
+            
+            for(int i=1; i < n; i++)
+            {
+                mul *= i;
+            }
+            await Task.Delay(1000);            
+            return mul;
+        }
+      public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Task<int> s= Sum(n);
+            Task<int> p =  Mul(n);
+            var prod = await p;
+            Console.WriteLine(prod);
+            var sum = await s;            
+            Console.WriteLine(sum);                        
+        }
+    }
+}
--- Cgdemo.cs
+++ Cgdemo.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Array
 {
+    public class Tuplecg
+    {
+        public int Id;
+        public string Name;
+        public double Salary;
+    }
     internal class Cgdemo
     {
+        static void Main(string[] args)
+        {
+            List<(int,string,double)> list = new List<(int,string,double)>
+            {
+                (101,"Siva",20000),
+                (102,"Arun",21000),
+                (103,"Srikanth",22000)
+            };
+        }
     }
 }
