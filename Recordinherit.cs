c692acbc4c6f2518b57ef18a8abdfa5fcba5cf03
SivaBharat <sivabharthece@gmail.com>
2023-08-16 12:41:11 +0530

    recordCommit

---
+++ Recordinherit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    public record class Players
+    {
+        public void display()
+        {
+            Console.WriteLine("Players category");
+        }
+    }
+    public record class Grade1 : Players
+    {
+        public void display1()
+        {
+            Console.WriteLine("Grade one players");
+        }
+    }
+    public record class Grade2 : Players
+    {
+        public void display2()
+        {
+            Console.WriteLine("Grade two players");
+        }
+    }
+    internal class Recordinherit
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Base class record");
+            Players p=new Players();
+            p.display();
+            Console.WriteLine("First Child class record");
+            Grade1 g=new Grade1();
+            g.display();
+            g.display1();
+            Console.WriteLine("Second child class record");
+            Grade2 grade2 = new Grade2();
+            grade2.display();
+            grade2.display2();
+        }
+    }
+}
---
+++ Recordstructdemo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { }
+    public record struct Employee
+    {
+        public int ID { get; init; }
+        public string Name { get; init; }       
+        public double Salary;
+        public double Bonus;
+        public static void Display(Employee employee)
+        {
+            if (employee.Salary > 50000)
+            {
+                employee.Bonus = (employee.Salary * 20) / 100;
+                employee.Salary += employee.Bonus;
+            }
+            else if (employee.Salary < 50000 && employee.Salary >= 25000)
+            {
+                employee.Bonus = (employee.Salary * 15) / 100;
+                employee.Salary += employee.Bonus;
+            }
+            else
+            {
+                employee.Bonus = (employee.Salary * 10) / 100;
+                employee.Salary += employee.Bonus;
+            }            
+            Console.WriteLine("\nEmployee Details: ");
+            Console.WriteLine($"Employee ID: {employee.ID}");
+            Console.WriteLine($"Employee Name: {employee.Name}");            
+            Console.WriteLine($"Bonus: {employee.Bonus}");
+            Console.WriteLine($"Total salary with bonus: {employee.Salary}");
+        }
+    }
+    internal class Recordstructdemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of Employees");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter Employee details:");
+                Console.WriteLine($"Enter the {i+1}th Employee ID");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Enter the {i+1}th Employee Name");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Enter the {i+1}th employee Designation");
+                string des = Console.ReadLine();
+                Console.WriteLine($"Enter the {i+1}th employee Salary");
+                double sal = Convert.ToDouble(Console.ReadLine());
+                var empRec = new Employee
+                {
+                    ID = id,
+                    Salary = sal,
+                    Name = name,                    
+                };
+                Employee.Display(empRec);
+                Console.WriteLine("\n------------------------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
